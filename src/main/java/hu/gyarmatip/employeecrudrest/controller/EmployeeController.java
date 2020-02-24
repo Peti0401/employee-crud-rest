d911d49347965bf6255722253f184e192224e90e
Péter Gyarmati <gyarmatipeti01@gmail.com>
2020-02-24 18:50:49 +0100

    Base entity created with corresponding Repository interface and RestController class with save and findAll functions.

---
+++ src/main/java/hu/gyarmatip/employeecrudrest/controller/EmployeeController.java
@@ -0,0 +1,34 @@
+package hu.gyarmatip.employeecrudrest.controller;
+
+import hu.gyarmatip.employeecrudrest.entity.Employee;
+import hu.gyarmatip.employeecrudrest.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController("/api")
+public class EmployeeController {
+
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @PostMapping("/employee")
+    public Employee save(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @GetMapping("/employee")
+    public List<Employee> findAll() {
+        return ((List<Employee>) employeeRepository.findAll());
+    }
+
+}
